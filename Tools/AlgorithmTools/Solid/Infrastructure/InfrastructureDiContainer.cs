8c051f6df2677094107a7feb5da36e4976604834
Hanke, Steffen <steffen.hanke.ext@siemens-healthineers.com>
2023-01-09 17:44:54 +0100

    renamed  BasicInfrastructureDiContainer class

--- Tools/AlgorithmTools/Solid/Infrastructure/InfrastructureDiContainer.cs
+++ Tools/AlgorithmTools/Solid/Infrastructure/InfrastructureDiContainer.cs
@@ -1,15 +1,15 @@
 ﻿//----------------------------------------------------------------------------------
-// <copyright file="BasicInfrastructureDiContainer.cs" company="Siemens Healthcare GmbH">
-// Copyright (C) Siemens Healthcare GmbH, 2022. All Rights Reserved. Confidential.
+// <copyright file="InfrastructureDiContainer.cs" company="Siemens Healthcare GmbH">
+// Copyright (C) Siemens Healthcare GmbH, 2022-2023. All Rights Reserved. Confidential.
 // Author: Steffen Hanke
 // </copyright>
 //----------------------------------------------------------------------------------
 
 namespace Solid.Infrastructure
 {
-    public class BasicInfrastructureDiContainer : DiContainer.Impl.DiContainer
+    public class InfrastructureDiContainer : DiContainer.Impl.DiContainer
     {
-        public BasicInfrastructureDiContainer()
+        public InfrastructureDiContainer()
         {
             this.Register(new InfrastructureRegistrar());
         }
